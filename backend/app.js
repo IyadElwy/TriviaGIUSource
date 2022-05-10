2cc9653b8cfe1b4c49709a01764beffee9358532
Iyad Elwy <iyadelwy1@gmail.com>
2022-05-10 17:43:26 +0200

    Session2

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fetch = require('node-fetch');
+///////////////////////////////////////////////////////////////////////////////
+
+
+const app = express();
+const port = 3000;
+
+const router = express.Router();
+///////////////////////////////////////////////////////////////////////////////
+
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+router.get('/startgame', async (req, res) => {
+    const result = await fetch('https://opentdb.com/api.php?amount=50&type=boolean');
+    const x = await result.json();
+    res.send(x);
+});
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+app.use(router);
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`)
+});
+
